ee284b3f4dd910cc5759df691da1c150727a00b1
newbee-Rjt <65074554+newbee-Rjt@users.noreply.github.com>
2022-09-23 10:06:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -90,6 +90,38 @@ export const config = {
         {"keyword": "love_day", date: "2022-03-17"},
       ]
     },
+    {
+      // 想要发送的人的名字
+      name: "我自己",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "ogPlX68D_ywEk0QXM_CGWxl8SNSs",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "pGmWHoueCzHnqsI21FxEjkvjt2UmTB9XASREj5WYiA0",
+      // 所在省份
+      province: "河北",
+      // 所在城市
+      city: "邯郸",
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '11-05',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://baidu.com",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+//         {"type": "*生日", "name": "", "year": "2001", "date": "11-05"},
+//         {"type": "节日", "name": "恋爱纪念日", "year": "2022", "date": "03-17"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {"type": "生日", "name": "你", "year": "2001", "date": "11-05"},
+        {"type": "节日", "name": "恋爱纪念日", "year": "2022", "date": "03-17"},
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        {"keyword": "love_day", date: "2022-03-17"},
+      ]
+    },
   ],
 
   /**
